5dffa712c838f4aa7a443396a692d410091ff38b
Oleksii Skrypka <oleksii.skrypka@globallogic.com>
2018-12-17 18:16:38 +0200

    Add multicore boot code

--- rpi3_boot/src/lib.rs
+++ rpi3_boot/src/lib.rs
@@ -1,7 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![no_std]
+#![feature(global_asm)]
+
+//! Low-level boot of the Raspberrys's processor
+
+extern crate panic_abort;
+extern crate r0;
+
+#[macro_export]
+macro_rules! entry {
+    ($path:path) => {
+        #[export_name = "main"]
+        pub unsafe fn _main() -> ! {
+            // type check the given path
+            let f: fn() -> ! = $path;
+
+            f()
+        }
+    };
 }
+
+/// Reset function
+///
+/// Initializes the bss section before calling into the user's `main()`.
+#[no_mangle]
+pub unsafe extern "C" fn reset() -> ! {
+    extern "C" {
+        // Boundaries of the .bss section, provided by the linker script
+        static mut __bss_start: u64;
+        static mut __bss_end: u64;
+    }
+
+    // Zeroes the .bss section
+    r0::zero_bss(&mut _bss_start, &mut _bss_end);
+
+    extern "Rust" {
+        fn main() -> !;
+    }
+
+    main();
+}
+
+// Disable all cores except core 0, and then jump to reset()
+global_asm!(include_str!("boot_cores.S"));
